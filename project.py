6e237d5ef94a2b106e888e0728e9ab1704ea45c9
malaravank <113099610+malaravank@users.noreply.github.com>
2022-09-09 12:36:25 +0530

    Create project.py

---
+++ project.py
@@ -0,0 +1,13 @@
+k=int(input("Enter The Marks : "))
+if k > 35:
+    print("You Pass The Exam")
+else:
+    print("You Fail The Exam")"""
+"""i = 1
+while i<=5:
+     print(i)"""
+"""
+for i in range(2):
+     f=str(input("Enter the first name :"))
+     s=str(input("Enter second fi name :"))
+     print(f,s)
